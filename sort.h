1107318095a559b8f995578cbe3119f5eecaf42b
gevalinho <gevalinho1@gmail.com>
2023-02-20 14:43:22 +0100

    sort.h

--- sort.h
+++ sort.h
@@ -9,28 +9,27 @@
 #define DOWN 1
 
 /**
- *  * enum bool - Enumeration of Boolean values.
- *   * @false: Equals 0.
- *    * @true: Equals 1.
- *     */
+ *  enum bool - Enumeration of Boolean values.
+ *  @false: Equals 0.
+ *  @true: Equals 1.
+ */
 typedef enum bool
 {
-    false = 0,
-    true
+false = 0,
+true
 } bool;
 
 /**
- *  * struct listint_s - Doubly linked list node
- *   *
- *    * @n: Integer stored in the node
- *     * @prev: Pointer to the previous element of the list
- *      * @next: Pointer to the next element of the list
- *       */
+ *  struct listint_s - Doubly linked list node
+ *  @n: Integer stored in the node
+ *  @prev: Pointer to the previous element of the list
+ *  @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /* Printing helper functions */
